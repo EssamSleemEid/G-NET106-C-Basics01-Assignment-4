cf3d00507423f595c42dbfcd6083db8b69c939b6
Nongoogle <essamsleem9@gmail.com>
2026-09-05 21:20:51 +0300

    Question02

--- Program.cs
+++ Program.cs
@@ -7,8 +7,16 @@
             #region Question1
             //Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print the second price (index 1). 
 
-            double[] prices = { 25.5, 40.0, 33.75 };
-            Console.WriteLine("the second pricr is :" + prices[1]);
+            //double[] prices = { 25.5, 40.0, 33.75 };
+            //Console.WriteLine("the second pricr is :" + prices[1]);
+            #endregion
+
+            #region Question2
+            //Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0
+
+            //int[,] shelfCopies = { { 3 , 5 } , { 1 , 4 } };
+
+            //Console.WriteLine("shelf 1 slot 0 : " + shelfCopies[1,0]);
             #endregion
         }
     }
