a340caddc9a7dbc930b667e372cf38d47b3d6bea
Nongoogle <essamsleem9@gmail.com>
2026-09-05 22:15:38 +0300

    Question12

--- Program.cs
+++ Program.cs
@@ -130,6 +130,19 @@
 
             //PrintBookInfo(pages: 600, title: "essam");
             #endregion
+
+            #region Question12
+            //Write a method PrintAllTitles(params string[] titles) that prints each title on its own line. Call it with three book titles
+
+            //static void PrintAllTitles(params string[] titles)
+            //{
+            //    for (int i = 0; i <= titles.Length; i++)
+            //    {
+            //        Console.WriteLine("title : " + titles[i]);
+            //    }
+            //}
+            //PrintAllTitles("essam", "route", "nice");
+            #endregion
         }
     }
 }
